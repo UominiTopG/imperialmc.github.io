6528fa9f31211c2c51f9f53b73a44487894948b0
Pino <mrcreeper1216@gmail.com>
2023-02-25 17:57:53 +0100

    Add files via upload

---
+++ imperial/js/modal.js
@@ -0,0 +1,21 @@
+var modal = document.getElementById('modal');
+var btn = document.getElementById('clickModal');
+var close = document.getElementsByClassName('close')[0];
+
+btn.addEventListener('click', openModal);
+/* close.addEventListener('click', closeModal); */
+window.addEventListener('click', wClose);
+
+function openModal(){
+    var text = document.getElementById('copy').innerHTML;
+    navigator.clipboard.writeText(text);
+    modal.style.display = 'block';
+}
+function closeModal(){
+    modal.style.display = 'none';
+}
+function wClose(e){
+    if(e.target == modal){
+        modal.style.display = 'none';
+    }
+}
---
+++ imperial/js/slidergrab.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".swiper-cont", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    centerSlide: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        700: {
+            slidesPerView: 2,
+        },
+        1100: {
+            slidesPerView: 3,
+        },
+    },
+  });
