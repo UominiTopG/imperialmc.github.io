a9c6edf083f189aa65f2c088c9c2f2f346eca956
ChExpress3 <simodoganieri@hotmail.it>
2023-03-06 00:56:59 +0100

    Updates

--- js/main.js
+++ js/main.js
@@ -18,15 +18,3 @@ function IplDiscord() {
       document.getElementById("players-ds").innerHTML = data.presence_count
     ));
 }
-
-$(document).ready(function () {
-  $('a[href^="#"]').on('click', function (event) {
-    var target = $(this.getAttribute('href'));
-    if (target.length) {
-      event.preventDefault();
-      $('html, body').stop().animate({
-        scrollTop: target.offset().top
-      }, 1000);
-    }
-  });
-});
