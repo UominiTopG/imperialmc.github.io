22e03e307f73d87c5e8b0cd6df467babbf2ee55f
ChExpress3 <simodoganieri@hotmail.it>
2023-03-06 14:10:06 +0100

    Update

--- js/main.js
+++ js/main.js
@@ -18,3 +18,49 @@ function IplDiscord() {
       document.getElementById("players-ds").innerHTML = data.presence_count
     ));
 }
+
+
+/* Modal */
+var modal = document.getElementById('modal');
+var btn = document.getElementById('clickModal');
+var close = document.getElementsByClassName('close')[0];
+
+btn.addEventListener('click', openModal);
+window.addEventListener('click', wClose);
+
+function openModal() {
+    var text = document.getElementById('copy').innerHTML;
+    navigator.clipboard.writeText(text);
+    modal.style.display = 'block';
+}
+function closeModal() {
+    modal.style.display = 'none';
+}
+function wClose(e) {
+    if (e.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+
+/* Slider grab */
+
+var swiper = new Swiper(".swiper-cont", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  centerSlide: true,
+  pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+  },
+  breakpoints: {
+      0: {
+          slidesPerView: 1,
+      },
+      700: {
+          slidesPerView: 2,
+      },
+      1100: {
+          slidesPerView: 3,
+      },
+  },
+});
